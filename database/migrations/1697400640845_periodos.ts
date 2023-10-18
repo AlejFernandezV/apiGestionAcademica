c4eef06778e61421c7e46e5c56102dfce1dab36e
AlejFernandezV <alejfernandez@unicauca.edu.co>
2023-10-18 18:28:05 -0500

    Migrations complete

--- database/migrations/1697400640845_periodos.ts
+++ database/migrations/1697400640845_periodos.ts
@@ -1,7 +1,7 @@
 import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 
 export default class extends BaseSchema {
-  protected tableName = 'periodos'
+  protected tableName = 'periodo'
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
--- database/migrations/1697400677635_evaluaciones.ts
+++ database/migrations/1697400677635_evaluaciones.ts
@@ -5,9 +5,10 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('eva_id').unsigned()
+      table.increments('eva_id')
       table.integer('lab_id').unsigned().references('lab_id').inTable('labor')
       table.integer('per_id').unsigned().references('per_id').inTable('periodo')
+      table.integer('usu_id').unsigned().references('usu_id').inTable('usuario_rol')
       table.smallint('eva_estado')
       table.float('eva_puntaje', 4,2)
       table.string('eva_resultado', 1000)
