cc55a8501ddb9047baa2e4f296c0abb8791d2153
AlejFernandezV <alejfernandez@unicauca.edu.co>
2023-10-21 16:34:04 -0500

    Adding validators in migrations

--- database/migrations/1697400640844_labores.ts
+++ database/migrations/1697400640844_labores.ts
@@ -6,8 +6,8 @@ export default class extends BaseSchema {
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
       table.increments('lab_id')
-      table.string('lab_nombre', 50)
-      table.integer('lab_horas')
+      table.string('lab_nombre', 50).notNullable()
+      table.integer('lab_horas').notNullable()
       table.integer('tl_id').unsigned().references('tl_id').inTable('tipo_labor')
       table.timestamps()
     })
