7b732139c7c0f13dd8cea5341a40ce104d547ea5
AlejFernandezV <alejferandez@unicauca.edu.co>
2023-11-21 20:30:19 -0500

    Finishing Facade Evaluation

--- app/Controllers/Http/EvaluacionesController.ts
+++ app/Controllers/Http/EvaluacionesController.ts
@@ -9,16 +9,29 @@ import UsuarioRol from 'App/Models/UsuarioRol/UsuarioRolModel'
 
 export default class EvaluacionesController {
 
-  public async index({}: HttpContextContract) {
-    return await Evaluacion.all()
+  public async index() {
+    return await Database
+    .from('evaluacion')
+    .join('usuario_rol','evaluacion.usu_id','usuario_rol.usu_id')
+    .join('periodo','evaluacion.per_id','periodo.per_id')
+    .join('labor', 'evaluacion.lab_id','labor.lab_id')
+    .join('tipo_labor','labor.tl_id','tipo_labor.tl_id')
+    .select(
+      'labor.lab_nombre',
+      'tipo_labor.tl_descripcion',
+      'labor.lab_horas',
+      //CONSULTA PENDIENTE PARA LA DESCRIPCIÓN
+      'periodo.per_fecha_inicio',
+      'periodo.per_fecha_fin',
+      'evaluacion.eva_estado',
+      'evaluacion.eva_resultado',
+      'evaluacion.eva_puntaje'
+    )
+
   }
 
-  public async indexByDocente({request, response}: HttpContextContract) {
-    const {idDocente} = request.all()
-
-    const api = new Api()
-
-    const results = await Database
+  public async indexByDocente(idDocente: number) {
+    return await Database
     .from('evaluacion')
     .join('usuario_rol','evaluacion.usu_id','usuario_rol.usu_id')
     .join('periodo','evaluacion.per_id','periodo.per_id')
@@ -36,23 +49,10 @@ export default class EvaluacionesController {
       'evaluacion.eva_puntaje'
     )
     .where('usuario_rol.usu_id','=',idDocente)
-
-    if(results.length > 0){
-      api.setResult(results)
-    }
-    else{
-      api.setState(404,"Error", "No hubo resultados para ese docente")
-    }
-
-    return response.json(api.toResponse())
   }
 
-  public async indexByPeriodo({request, response}: HttpContextContract) {
-    const {nombrePeriodo} = request.all()
-
-    const api = new Api()
-
-    const results = await Database
+  public async indexByPeriodo(nombrePeriodo: string) {
+    return await Database
     .from('evaluacion')
     .join('usuario_rol','evaluacion.usu_id','usuario_rol.usu_id')
     .join('periodo','evaluacion.per_id','periodo.per_id')
@@ -70,61 +70,20 @@ export default class EvaluacionesController {
       'evaluacion.eva_puntaje'
     )
     .where('periodo.per_nombre','like',nombrePeriodo)
-
-    if(results.length > 0){
-      api.setResult(results)
-    }
-    else{
-      api.setState(404,"Error", "No hubo resultados para ese periodo")
-    }
-
-    return response.json(api.toResponse())
   }
 
-  public async store({request, response}: HttpContextContract) {
-    const {eva_estado, eva_puntaje, eva_resultado, lab_id, per_id, usu_id} = request.all()
-
-    const labor = await Labor.findByOrFail('lab_id',lab_id)
-    const periodo = await Periodo.findByOrFail('per_id',per_id)
-    const usuario = await UsuarioRol.findByOrFail('usu_id',usu_id)
-    const api = new Api()
-    const newEval = new Evaluacion()
-
-    if(eva_estado != null &&
-      eva_puntaje != null &&
-      eva_resultado != null &&
-      lab_id != null &&
-      per_id != null &&
-      usu_id != null){
-
-        newEval.eva_estado = eva_estado
-        newEval.eva_puntaje = eva_puntaje
-        newEval.eva_resultado = eva_resultado
-        newEval.lab_id = labor.lab_id
-        newEval.per_id = periodo.per_id
-        newEval.usu_id = usuario.usu_id
-        const results = await newEval.save()
-        api.setResult(results)
-    }
-    else{
-      api.setState(404,"Error", "No se pudo crear la evaluacion")
-    }
-    return response.json(api.toResponse())
+  public async store(evaluacion: Evaluacion) {
+    return await Evaluacion.create(evaluacion)
   }
 
-  public async updateResult({request, response}: HttpContextContract) {
-    const api = new Api()
-    const cuerpo = request.only(['eva_id','eva_resultado'])
-
-    const evaluacion = await Evaluacion.findByOrFail("eva_id",cuerpo.eva_id)
-
-    if(evaluacion!=null){
-      const results = await evaluacion.merge(cuerpo).save()
-      api.setResult(results)
-    }else{
-      api.setState(404,"Error", "No se pudo actualizar el resultado de la evaluacion")
+  public async updateResult(data: any) {
+    try{
+      const evaluacion = await Evaluacion.findByOrFail("eva_id",data.eva_id)
+      return await evaluacion.merge(data).save()
+    }catch(error){
+      console.log(error)
+      return null
     }
-    return response.json(api.toResponse())
   }
 
   public async destroy({request, response}: HttpContextContract) {
--- app/Facades/FachadaEvaluacion.ts
+++ app/Facades/FachadaEvaluacion.ts
@@ -1,8 +1,114 @@
 import type { HttpContextContract} from '@ioc:Adonis/Core/HttpContext'
 import EvaControlador from 'App/Controllers/Http/EvaluacionesController'
 import Evaluacion  from 'App/Models/Evaluacion/EvaluacionModel'
+import Labor from 'App/Models/Labor/LaborModel'
+import Periodo from 'App/Models/Periodo/PeriodoModel'
+import UsuarioRol from 'App/Models/UsuarioRol/UsuarioRolModel'
 import Api from 'App/Helpers/ResponseApi'
 
 export default class FachadaEvaluacion{
+  public async listarEvaluaciones({response}: HttpContextContract){
+    const api = new Api()
+    const evaControlador = new EvaControlador()
 
+    const results = await evaControlador.index()
+
+    if(results.length <= 0){
+      api.setState(404,"Error","No hay evaluaciones para listar")
+    }else{
+      api.setResult(results)
+    }
+    return response.json(api.toResponse())
+  }
+
+  public async listarEvaluacionesPorDocente({request, response}: HttpContextContract){
+    const api = new Api()
+    const evaControlador = new EvaControlador()
+    const idDocente = request.input("id")
+
+    const results = await evaControlador.indexByDocente(idDocente)
+
+    if(results.length <= 0){
+      api.setState(404,"Error","No hay evaluaciones de este profesor para listar")
+    }else{
+      api.setResult(results)
+    }
+    return response.json(api.toResponse())
+  }
+
+  public async listarEvaluacionesPorPeriodo({request, response}: HttpContextContract){
+    const api = new Api()
+    const evaControlador = new EvaControlador()
+    const periodoNombre = request.input("nombre")
+
+    const results = await evaControlador.indexByPeriodo(periodoNombre)
+
+    if(results.length <= 0){
+      api.setState(404,"Error","No hay evaluaciones en este periodo para listar")
+    }else{
+      api.setResult(results)
+    }
+    return response.json(api.toResponse())
+  }
+
+  public async crearEvaluacion({request, response}: HttpContextContract){
+    const api = new Api()
+    const data = request.only(['eva_estado', 'eva_puntaje', 'eva_resultado', 'lab_id', 'per_id', 'usu_id'])
+    const evaControlador = new EvaControlador()
+    const evaluacion = new Evaluacion()
+
+    if(data.eva_estado === null){
+      api.setState(404,"Error","No se pudo crear la evaluacion, el estado no puede ser nulo")
+      return response.json(api.toResponse())
+    }
+    if(data.eva_puntaje === null){
+      api.setState(404,"Error","No se pudo crear la evaluacion, el puntaje no puede ser nulo")
+      return response.json(api.toResponse())
+    }
+    if(data.eva_resultado === null){
+      api.setState(404,"Error","No se pudo crear la evaluacion, el resultado no puede ser nulo")
+      return response.json(api.toResponse())
+    }
+
+    try{
+      const labor = await Labor.findByOrFail('lab_id',data.lab_id)
+      const periodo = await Periodo.findByOrFail('per_id',data.per_id)
+      const usuario = await UsuarioRol.findByOrFail('usu_id',data.usu_id)
+
+      evaluacion.eva_estado = data.eva_estado
+      evaluacion.eva_puntaje = data.eva_puntaje
+      evaluacion.eva_resultado = data.eva_resultado
+      evaluacion.lab_id = labor.lab_id
+      evaluacion.per_id = periodo.per_id
+      evaluacion.usu_id = usuario.usu_id
+
+      const results = await evaControlador.store(evaluacion)
+
+      if(results == null){
+        api.setState(404,"Error","No se pudo crear la evaluacion")
+      }else{
+        api.setResult(results)
+      }
+
+    }catch(error){
+      console.log(error)
+    }finally{
+      return response.json(api.toResponse())
+    }
+  }
+
+  public async actualizarResultado({request, response}: HttpContextContract){
+    const api = new Api()
+    const data = request.only(['eva_id','eva_resultado'])
+    const evaControlador = new EvaControlador()
+    const evaluacion = await Evaluacion.findByOrFail("eva_id",data.eva_id)
+
+    if(evaluacion===null){
+      api.setState(404,"Error", "No se pudo actualizar el resultado de la evaluacion")
+    }else{
+      const results = await evaControlador.updateResult(data)
+      api.setResult(results)
+    }
+    return response.json(api.toResponse())
+  }
 }
