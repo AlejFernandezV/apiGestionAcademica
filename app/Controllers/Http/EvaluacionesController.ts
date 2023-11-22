b596ec54b9db928cb84ca0c5d7ce83a0ab83c0df
AlejFernandezV <alejfernandez@unicauca.edu.co>
2023-11-21 23:13:17 -0500

    Fixing errors before production

--- app/Controllers/Http/EvaluacionesController.ts
+++ app/Controllers/Http/EvaluacionesController.ts
@@ -2,10 +2,6 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Database from '@ioc:Adonis/Lucid/Database'
 import Api from 'App/Helpers/ResponseApi'
 import Evaluacion from 'App/Models/Evaluacion/EvaluacionModel'
-import Labor from 'App/Models/Labor/LaborModel'
-import Periodo from 'App/Models/Periodo/PeriodoModel'
-import UsuarioRol from 'App/Models/UsuarioRol/UsuarioRolModel'
-
 
 export default class EvaluacionesController {
 
